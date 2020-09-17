53681a7b74521e60d8692f33e748db65ae366a70
kushanz <kushanshanuka@yahoo.com>
2020-09-17 12:17:48 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-udemy-try
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-udemy-try)
---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent implements OnInit  {
+  name = 'Angular';
+  posts:any[];
+  private url = 'https://jsonplaceholder.typicode.com/posts'
+
+  constructor(private http: HttpClient) {
+
+  }
+  ngOnInit() {
+    this.http.get<Post[]>(this.url).subscribe(response => {
+      this.posts =  response;
+    })
+  }
+}
+
+export class Post {
+    id:         number;
+    userId:     number;
+    title:      string;
+    body:       string;
+}
